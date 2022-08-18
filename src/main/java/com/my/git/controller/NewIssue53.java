5307c5082903480360e5dee969b43cd195fdb777
Honest.Zhao <Zmc$18301112>
2022-08-18 22:05:33 +0800

    fast hot fix finished and switch to issue branch

---
+++ src/main/java/com/my/git/controller/NewIssue53.java
@@ -0,0 +1,12 @@
+package com.my.git.controller;
+
+/**
+ * Description: description of NewIssue53
+ * Company: 联宝（合肥）电子科技有限公司
+ *
+ * @author: Honest.Zhao
+ * Date: 2022/8/18 22:04
+ */
+
+public class NewIssue53 {
+}
