6ad592af21580aeb7dea964725eafce3e42686d7
Honest.Zhao <Zmc$18301112>
2022-08-18 21:54:42 +0800

    fix issue 53

---
+++ src/main/java/com/my/git/controller/Issue53.java
@@ -0,0 +1,13 @@
+package com.my.git.controller;
+
+/**
+ * Description: description of Issue53
+ * Company: 联宝（合肥）电子科技有限公司
+ *
+ * @author: Honest.Zhao
+ * Date: 2022/8/18 21:53
+ */
+
+public class Issue53 {
+
+}
