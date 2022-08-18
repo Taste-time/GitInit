1da5a3a70dccffeeb8a89af5377277a4d21a9b4f
Honest.Zhao <Zmc$18301112>
2022-08-18 23:00:12 +0800

    new branch

---
+++ src/main/java/com/my/git/controller/NewBranch.java
@@ -0,0 +1,12 @@
+package com.my.git.controller;
+
+/**
+ * Description: description of NewBranch
+ * Company: 联宝（合肥）电子科技有限公司
+ *
+ * @author: Honest.Zhao
+ * Date: 2022/8/18 22:59
+ */
+
+public class NewBranch {
+}
